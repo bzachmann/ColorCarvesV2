578d8ec3111023401e90e983be49914ed4fd4caf
bzachmann <boston.a.zachmann@ndsu.edu>
2017-08-14 21:30:34 -0500

    Changed ANGSET msg to receive absolute mode

--- CMPRxANGSET.cpp
+++ CMPRxANGSET.cpp
@@ -10,6 +10,8 @@
 #include "apmain.h"
 
 #define SCALE_CMP_ANGLE		(0.1f)
+#define ANGLE_DC			(0x03FF)
+#define ABS_MODE_DC			(0x03)
 
 CMPRxANGSET::CMPRxANGSET() :
 	CMPDataHandler()
@@ -19,9 +21,26 @@ CMPRxANGSET::CMPRxANGSET() :
 
 void CMPRxANGSET::callback(CMPData * data)
 {
-	uint16_t tempWord = (((uint16_t)(data->getByte(1) & 0x03)) << 8) | (data->getByte(0) & 0xFF);
-	float newAngleLimit = tempWord * SCALE_CMP_ANGLE;
-	ApMain::inst.tiltSensor.setAngleLimit(newAngleLimit);
+	uint16_t angleLimitWord = (((uint16_t)(data->getByte(1) & 0x03)) << 8) | (data->getByte(0) & 0xFF);
+	uint8_t absoluteMode = (data->getByte(2) & 0x03);
+
+	if(angleLimitWord != ANGLE_DC)
+	{
+		float newAngleLimit = angleLimitWord * SCALE_CMP_ANGLE;
+		ApMain::inst.tiltSensor.setAngleLimit(newAngleLimit);
+	}
+
+	if(absoluteMode != ABS_MODE_DC)
+	{
+		if(absoluteMode == 0)
+		{
+			ApMain::inst.tiltSensor.setAbsoluteMode(false);
+		}
+		else if(absoluteMode == 1)
+		{
+			ApMain::inst.tiltSensor.setAbsoluteMode(true);
+		}
+	}
 }
 
 void CMPRxANGSET::init()
